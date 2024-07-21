b1cf3577c4b76299c1970723d44b138eb38e5425
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2024-07-21 18:40:51 +0300

    sprint04 week03 task3-4: show errors

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -1,16 +1,22 @@
 import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
-import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
+import {
+  addDeckAC,
+  deleteDeckAC,
+  setDecksAC,
+  updateDeckAC,
+} from './decks-reducer.ts'
 import { setAppStatusAC } from '../../app/app-reducer.ts'
+import { isAxiosError } from 'axios'
 
 export const fetchDecksTC = () => async (dispatch: Dispatch) => {
   dispatch(setAppStatusAC('loading'))
   try {
-    const res = await decksAPI.fetchDecks();
-    dispatch(setDecksAC(res.data.items));
+    const res = await decksAPI.fetchDecks()
+    dispatch(setDecksAC(res.data.items))
     dispatch(setAppStatusAC('succeeded'))
   } catch (error) {
-    console.warn(error);
+    console.warn(error)
     dispatch(setAppStatusAC('failed'))
   }
   dispatch(setAppStatusAC('idle'))
@@ -28,8 +34,31 @@ export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
   })
 }
 
+// case 1 - error on backend (console.log / network or log /
+// axiosError.response.data.errorMessage[0].message
+
+// case 2 - error on client side (console.log / network or log /
+// // axiosError.message
+
+// case 3 - error in code (поле e.message)
+
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
+  try {
+    const res = await decksAPI.updateDeck(params)
     dispatch(updateDeckAC(res.data))
-  })
+  } catch (e) {
+    let errorMessage: string
+
+    if (isAxiosError<ServerErrorMessageType>(e)) {
+      errorMessage = e.response ? e.response.data.errorMessages[0].message : e.message
+    } else {
+      errorMessage = (e as Error).message
+    }
+    console.log(errorMessage)
+  }
 }
+
+type ServerErrorMessageType = {
+  errorMessages: Array<{field: string,message: string}>
+}
+
